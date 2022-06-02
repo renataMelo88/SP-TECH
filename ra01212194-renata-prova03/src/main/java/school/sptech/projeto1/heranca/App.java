9f0d162a27182692142fad61f48cfe2733a82802
renata.melo <renata.melo@linx.com.br>
2022-06-02 20:22:08 -0300

    atualizando projeto-lista

---
+++ ra01212194-renata-prova03/src/main/java/school/sptech/projeto1/heranca/App.java
@@ -0,0 +1,11 @@
+
+package school.sptech.projeto1.heranca;
+
+
+public class App {
+    public static void main(String[] args) {
+        //Estânciar classes 
+        Carrinho car1 = new Carrinho ("aGMax", 0);
+    }
+    
+}
